501058f087546e6d215d1d6e262d44bb8a1cd2f9
Thomas Gebert <thomasgebert@gmail.com>
2015-12-29 11:09:24 -0500

    Fix functions to call the correct splitter.

--- app/src/routing.erl
+++ app/src/routing.erl
@@ -4,23 +4,17 @@
 
 get_routes() ->
     JSON = <<"{\"GET /method\": \"mycontroller.action mycontroller.action2\"}">>,
-%%    Routes = jsx:decode(JSON, [return_maps]),
     Routes = jsx:decode(JSON),
     lists:map(fun create_cowboy_route/1, Routes).
-    %% lists:map(fun(Path) -> 
-    %%                   [Method, EndPoint] = string:token(Path, " "),
-    %%                   Actions = maps:get(Path
-    %%           end, Paths).
-    
 
 atomize(ModuleAction) ->
-    [Module, Action] = binary:token(ModuleAction),
+    [Module, Action] = binary:split(ModuleAction, <<".">>),
     ModAtom = list_to_atom(Module),
     ActAtom = list_to_atom(Action),
-    fun ModAtom:ModAtom/1.
+    fun ModAtom:ActAtom/1.
 
 create_cowboy_route({Path, ActionPath}) ->
-    [Method, EndPoint] = binary:token(Path, " "),
+    [Method, EndPoint] = binary:split(Path, <<" ">>),
     Actions = binary:token(ActionPath, " "),
     
     Methods = lists:map(fun atomize/1, Actions),
