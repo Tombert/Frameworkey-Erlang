19a2f7446d6f9171469d81a2e98ad6f704f427bd
Thomas Gebert <thomas@gebert.sexy>
2015-12-28 23:51:41 -0500

    Get rid of warnings, also remember to add the file for routing.

---
+++ app/src/routing.erl
@@ -0,0 +1,27 @@
+-module(routing).
+-export([splitter/0]).
+ 
+
+splitter() ->
+    JSON = <<"{\"GET /method\": \"mycontroller.action mycontroller.action2\"}">>,
+%%    Routes = jsx:decode(JSON, [return_maps]),
+    Routes = jsx:decode(JSON),
+    lists:map(fun create_cowboy_route/1, Routes).
+    %% lists:map(fun(Path) -> 
+    %%                   [Method, EndPoint] = string:token(Path, " "),
+    %%                   Actions = maps:get(Path
+    %%           end, Paths).
+    
+
+atomize(ModuleAction) ->
+    [Module, Action] = binary:token(ModuleAction),
+    fun Module:Action/1.
+
+create_cowboy_route({Path, ActionPath}) ->
+    [Method, EndPoint] = binary:token(Path, " "),
+    Actions = binary:token(ActionPath, " "),
+    
+    Methods = lists:map(fun atomize/1, Actions),
+    BigFunc = fn:multicompose(Methods),
+    {EndPoint, http_glue, [Method, BigFunc]}.
+    
