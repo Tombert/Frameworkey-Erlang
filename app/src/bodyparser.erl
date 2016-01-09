88bdecba6dc06d7d66665ec236ca9f7a2f098fc8
Thomas Gebert <thomas@gebert.sexy>
2016-01-08 22:34:54 -0500

    Pattern match so we have proper JSON parsing.

--- app/src/bodyparser.erl
+++ app/src/bodyparser.erl
@@ -4,16 +4,19 @@
 parse_body(Req) ->
     ContentType = cowboy_req:parse_header(<<"content-type">>, Req),
     {ok, Body, _} = cowboy_req:body(Req),
-    BodyMap = get_body_map(ContentType, Body),
-    io:format("My Body, gotta work, gotta work my body ~p~n~n", [BodyMap]),
-    {ok, KeyParams, Req2} = cowboy_req:body_qs(Req),
-    FormParamMap = maps:from_list(KeyParams),
-    QueryParamMap = maps:from_list(cowboy_req:parse_qs(Req2)),
-    maps:merge(maps:merge(QueryParamMap, FormParamMap), BodyMap).
+    BodyMap = get_body_map(ContentType, {Body, Req}),
+    io:format("My Body, gotta work, gotta work my body ~p~n~n", [ContentType]),
+
+    QueryParamMap = maps:from_list(cowboy_req:parse_qs(Req)),
+    maps:merge(QueryParamMap, BodyMap).
 
     
-get_body_map({<<"application">>,<<"json">>,_}, Body) ->
+get_body_map({<<"application">>,<<"json">>,_}, {Body, _}) ->
     jsx:decode(Body, [return_maps]);
+get_body_map({<<"application">>,<<"x-www-form-urlencoded">>,_},{_, Req}) ->
+    {ok, KeyParams, _} = cowboy_req:body_qs(Req),
+    maps:from_list(KeyParams);
+
 get_body_map(_,_) ->
     #{}.
 
