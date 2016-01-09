ca5703aca8ec7cab7913858658d5ccaf147e228d
Thomas Gebert <thomas@gebert.sexy>
2016-01-08 22:08:22 -0500

    Make it so we're parsing JSON.

--- app/src/bodyparser.erl
+++ app/src/bodyparser.erl
@@ -2,9 +2,18 @@
 -export([parse_body/1]).
 
 parse_body(Req) ->
+    ContentType = cowboy_req:parse_header(<<"content-type">>, Req),
+    {ok, Body, _} = cowboy_req:body(Req),
+    BodyMap = get_body_map(ContentType, Body),
+    io:format("My Body, gotta work, gotta work my body ~p~n~n", [BodyMap]),
     {ok, KeyParams, Req2} = cowboy_req:body_qs(Req),
     FormParamMap = maps:from_list(KeyParams),
     QueryParamMap = maps:from_list(cowboy_req:parse_qs(Req2)),
-    maps:merge(QueryParamMap, FormParamMap).
+    maps:merge(maps:merge(QueryParamMap, FormParamMap), BodyMap).
 
     
+get_body_map({<<"application">>,<<"json">>,_}, Body) ->
+    jsx:decode(Body, [return_maps]);
+get_body_map(_,_) ->
+    #{}.
+
