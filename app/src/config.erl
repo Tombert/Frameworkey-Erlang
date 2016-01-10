d3a52e7b7da31ae3588464d1d099964ba7ffd034
Thomas Gebert <thomas@gebert.sexy>
2016-01-10 18:12:04 -0500

    Change our config to an ETS table.  need to refactor slightly

--- app/src/config.erl
+++ app/src/config.erl
@@ -1,13 +1,19 @@
 -module(config). 
-
+-export([make_config/0, fetch/1]).
 
 make_config() ->
     {ok, JSON} = file:read_file("config.json"),
     ConfigMap = jsx:decode(JSON, [return_maps]),
-    create_download_dir(ConfigMap),
-    {ok, ConfigMap}.
+    {ok, DLString} = create_download_dir(ConfigMap),
+    ets:new(config_table, [named_table, protected, set, {keypos, 1}]),
+    ets:insert(config_table, {download_dir, DLString}).
 
+fetch(Field) ->
+    [{_, Value}] = ets:lookup(config_table, Field),    
+    Value.
+    
+    
 create_download_dir(ConfigMap) ->
     DLBin = maps:get(<<"download_tmp">>, ConfigMap),
     DLString = binary_to_list(DLBin),
-    files:ensure_dir(DLString).
+    {filelib:ensure_dir(DLString), DLString}.
