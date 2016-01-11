662a3524c9773c378cf679df3371c7ec360613e4
Thomas Gebert <thomas@gebert.sexy>
2016-01-10 20:53:43 -0500

    Stream files to disk : D

--- app/src/bodyparser.erl
+++ app/src/bodyparser.erl
@@ -3,22 +3,19 @@
 
 parse_body(Req) ->
     ContentType = cowboy_req:parse_header(<<"content-type">>, Req),
-    {ok, Body, _} = cowboy_req:body(Req),
-    BodyMap = get_body_map(ContentType, {Body, Req}),
-    io:format("My Body, gotta work, gotta work my body ~p~n~n", [ContentType]),
-
+    BodyMap = get_body_map(ContentType, Req),
     QueryParamMap = maps:from_list(cowboy_req:parse_qs(Req)),
     maps:merge(QueryParamMap, BodyMap).
 
     
-get_body_map({<<"application">>,<<"json">>,_}, {Body, _}) ->
+get_body_map({<<"application">>,<<"json">>,_}, Req) ->
+    {ok, Body, _} = cowboy_req:body(Req),
     jsx:decode(Body, [return_maps]);
-get_body_map({<<"application">>,<<"x-www-form-urlencoded">>,_},{_, Req}) ->
+get_body_map({<<"application">>,<<"x-www-form-urlencoded">>,_}, Req) ->
     {ok, KeyParams, _} = cowboy_req:body_qs(Req),
     maps:from_list(KeyParams);
-get_body_map({<<"multipart">>,<<"form-data">>,_},{_, Req}) ->
+get_body_map({<<"multipart">>,<<"form-data">>,_},Req) ->
     Things = multipart(Req),
-    io:format("~n~n~n~nThings ~p~n~n~n",[Things]),
     maps:from_list(Things);
 get_body_map(_,_) ->
     #{}.
@@ -30,23 +27,31 @@ multipart(Req)->
 multipart(Req, Stuff) ->
     case cowboy_req:part(Req) of
         {ok, Headers, Req2} ->
-            
             {Req4, NS} = case cow_multipart:form_data(Headers) of
-                       {data, _FieldName} ->
-                           
-                                       {ok, _Body, Req3} = cowboy_req:part_body(Req2),
-                                       NewStuff = [ {_FieldName, _Body} | Stuff],
-                                       {Req3, NewStuff}                                       
+                             {data, _FieldName} ->
+                                 {ok, _Body, Req3} = cowboy_req:part_body(Req2),
+                                 NewStuff = [ {_FieldName, _Body} | Stuff],
+                                 {Req3, NewStuff};
+                             {file, _FieldName, _Filename, _CType, _CTransferEncoding} ->
+
+                                 {ReqP, FileName} = stream_file(Req2, binary_to_list(_Filename)),
+                                 NewStuff = [ {_FieldName, list_to_binary(FileName)} | Stuff],
+                                 {ReqP, NewStuff}
+                                     
             end,
             multipart(Req4, NS);
         {done, Req2} ->
             Stuff
     end.
  
-stream_file(Req) ->
+stream_file(Req, FileName) ->
+    DLDir = config:fetch(download_dir),
+    FN = DLDir ++ FileName,
     case cowboy_req:part_body(Req) of
         {ok, _Body, Req2} ->
-            Req2;
+            file:write_file(DLDir ++ FileName, _Body, [append]),
+            {Req2, FN};
         {more, _Body, Req2} ->
-            stream_file(Req2)
+            file:write_file(DLDir ++ FileName, _Body,[append]),
+            stream_file(Req2, FN)
     end.
