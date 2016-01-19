eed04b8224e1cbfd1280aa13546d84793a9a47d8
Thomas Gebert <thomas@gebert.sexy>
2016-01-18 20:48:50 -0500

    Remove extra function.

--- app/apps/frameworkey/src/http_glue.erl
+++ app/apps/frameworkey/src/http_glue.erl
@@ -5,18 +5,11 @@
 init(Req, [HandlerMap]) ->
     Method = cowboy_req:method(Req),
     Params = bodyparser:parse_body(Req),
-    BigFunc = maybe_get_function(maps:is_key(Method, HandlerMap), HandlerMap, Method),
+    BigFunc = maps:get(Method, HandlerMap, fun(_) -> {json, #{error => <<"Not Found">>}} end),
     Value = BigFunc(Params),
     io:format("~n~n~n~p~n~n~n~n", [Value]),
     response(Value, Req),
     {ok, Req, [HandlerMap]}.
-
-maybe_get_function(false, _, _) ->
-    fun(_) ->
-            {json, #{error => <<"Not Found">>}}
-    end;
-maybe_get_function(true, Map, Method) ->
-    maps:get(Method, Map).
 
 
 response(#{header := Header, json := Data}, Req) ->
