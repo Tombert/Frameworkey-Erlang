093b60ded3f33a9e4fce1054063da0e519814956
Thomas Gebert <thomas@gebert.sexy>
2016-01-21 21:19:58 -0500

    Comment the http_glue function.

--- app/apps/frameworkey/src/http_glue.erl
+++ app/apps/frameworkey/src/http_glue.erl
@@ -14,14 +14,19 @@ init(Req, [HandlerMap]) ->
     {ok, Req, [HandlerMap]}.
 
 
+
+% This is a basic helper function because of my refusal to use a case statement. 
+% basically, if a "false" exists in the list, give back an error function. Otherwise, 
+% return back the original function. 
 authorizedFunc(true, _) -> 
     fun(_) ->
             #{json => #{error => <<"Not Authorized">>}, header => #{code => 403, data => []}}
     end;
 authorizedFunc(false, BigFunc) -> 
-    BigFunc.
+    BigFunc.nn
 
-
+% This function first normalizes the header and data, and then sends
+% back a response to the client. 
 response(#{header := Header, json := Data}, Req) ->
     #{ code := Code, data := RealHeader }= make_real_header(Header),
     Req2 = cowboy_req:reply(Code, RealHeader, jsx:encode(Data), Req);
@@ -29,6 +34,8 @@ response(#{json := Data}, Req) ->
     response(#{json => Data, header => #{ code => 200, data => [{<<"content-type">>, <<"application/json">>}]}}, Req).
 
 
+% This function takes in a header map, and normalizes
+% it to make sure it's formatted correctly
 make_real_header(#{code := Code, data := Data}) ->
     #{code => Code, data => Data};
 make_real_header(#{data := Data}) ->
